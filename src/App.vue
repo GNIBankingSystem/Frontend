0a077589bd977cf4f90e529e8d6162b412a1596d
CaptainJeppie <89974982+CaptainJeppie@users.noreply.github.com>
2023-05-10 11:42:42 +0200

    login page update

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,10 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img alt="GNI logo" class="logo" src="./assets/GNILogo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="GNI" />
     </div>
   </header>
 
